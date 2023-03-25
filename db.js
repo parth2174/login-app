5dd65f276a02caac214d61e2c977196cd2e6f580
Parth Sheth <parthsheth278@gmail.com>
2023-03-25 12:36:25 +0530

    modified file

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
 var mongoose = require('mongoose');
 //mongoose.connect('mongodb://localhost:27017/march7')
 
-mongoose.connect('mongodb+srv://parthsheth:parth5196@cluster0.zi1ko.mongodb.net/loginapp?retryWrites=true&w=majority";')
+mongoose.connect('mongodb+srv://parthsheth:parth5196@cluster0.zi1ko.mongodb.net/loginapp?retryWrites=true&w=majority')
